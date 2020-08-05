3632c7bdaf20fc6a2fd9810784f9e2ca0e55a464
Sebastian Gumprich <github@gumpri.ch>
2020-08-05 10:13:02 +0200

    add nginx tests

---
+++ molecule/nginx_hardening/tests/test_default.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Molecule managed
+
+describe file('/etc/hosts') do
+  its('owner') { should eq 'root' }
+  its('group') { should eq 'root' }
+  its('mode') { should cmp '0644' }
+end
