225f6ce2707999cdb9c67a7ff402ebe5b13399c3
Sebastian Gumprich <github@gumpri.ch>
2020-09-20 21:05:49 +0200

    remove useless tests directory

--- molecule/os_hardening/tests/test_default.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Molecule managed
-
-describe file('/etc/hosts') do
-  its('owner') { should eq 'root' }
-  its('group') { should eq 'root' }
-  its('mode') { should cmp '0644' }
-end
